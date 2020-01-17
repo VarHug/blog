d8f22209b9cec42458abd1dfce534db7910748c1
VarHug <490641570@qq.com>
2020-01-17 18:32:17 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>This is a simple blog</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
